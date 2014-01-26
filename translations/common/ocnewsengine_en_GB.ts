328d9e119bf557ee6af78475f3888048b1f7aee6
buschmann23 <buschmann@huessenbergnetz.de>
2014-01-26 15:12:57 +0100

    added en_GB to language files as source for transifex

---
+++ translations/common/ocnewsengine_en_GB.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_GB">
+<context>
+    <name>OcFeeds</name>
+    <message>
+        <location filename="../../engine/ocfeeds.cpp" line="33"/>
+        <location filename="../../engine/ocfeeds.cpp" line="246"/>
+        <location filename="../../engine/ocfeeds.cpp" line="420"/>
+        <location filename="../../engine/ocfeeds.cpp" line="545"/>
+        <location filename="../../engine/ocfeeds.cpp" line="732"/>
+        <source>Device is in flight mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocfeeds.cpp" line="73"/>
+        <location filename="../../engine/ocfeeds.cpp" line="311"/>
+        <source>Server reply was empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OcFolders</name>
+    <message>
+        <location filename="../../engine/ocfolders.cpp" line="13"/>
+        <location filename="../../engine/ocfolders.cpp" line="149"/>
+        <location filename="../../engine/ocfolders.cpp" line="228"/>
+        <location filename="../../engine/ocfolders.cpp" line="309"/>
+        <location filename="../../engine/ocfolders.cpp" line="389"/>
+        <source>Device is in flight mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocfolders.cpp" line="43"/>
+        <location filename="../../engine/ocfolders.cpp" line="189"/>
+        <source>Server reply was empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OcGeneric</name>
+    <message>
+        <location filename="../../engine/ocgeneric.cpp" line="35"/>
+        <location filename="../../engine/ocgeneric.cpp" line="98"/>
+        <source>Device is in flight mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocgeneric.cpp" line="66"/>
+        <location filename="../../engine/ocgeneric.cpp" line="130"/>
+        <source>Request canceled, maybe wrong username or password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocgeneric.cpp" line="68"/>
+        <location filename="../../engine/ocgeneric.cpp" line="132"/>
+        <source>Remote host not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocgeneric.cpp" line="70"/>
+        <location filename="../../engine/ocgeneric.cpp" line="134"/>
+        <source>Can not find ownCloud News, maybe wrong path or server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocgeneric.cpp" line="159"/>
+        <source>Server reply was empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OcItems</name>
+    <message>
+        <location filename="../../engine/ocitems.cpp" line="14"/>
+        <location filename="../../engine/ocitems.cpp" line="254"/>
+        <location filename="../../engine/ocitems.cpp" line="911"/>
+        <source>Device is in flight mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocitems.cpp" line="72"/>
+        <location filename="../../engine/ocitems.cpp" line="361"/>
+        <source>Server reply was empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OcUpdater</name>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="191"/>
+        <source>Your account is disabled or you have not created an account yet.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="227"/>
+        <source>Synchronizing ownCloud News</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="230"/>
+        <source>Updating Folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="280"/>
+        <source>Updating Feeds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="306"/>
+        <source>Updating Items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="347"/>
+        <source>Update Finished</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../engine/ocupdater.cpp" line="377"/>
+        <source>Update Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
