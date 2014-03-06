a276c599f5442c58d7adc09933307468bd4552f3
buschmann23 <buschmann@huessenbergnetz.de>
2014-03-06 15:13:01 +0100

    add russian translation by palacex

--- releasel10n.sh
+++ releasel10n.sh
@@ -4,7 +4,7 @@ STARTDIR=$PWD
 
 cd engine/l10n
 
-for LANG in de es fr it zh_CN
+for LANG in de es fr it ru zh_CN
 do
 lrelease ocnewsengine_$LANG.ts
 done
@@ -12,14 +12,14 @@ done
 cd ../../account/harmattan/l10n
 lrelease -markuntranslated '' -idbased ocnewsaccount.ts
 
-for LANG in de es fr it zh_CN
+for LANG in de es fr it ru zh_CN
 do
 lrelease -markuntranslated '' -idbased ocnewsaccount_$LANG.ts
 done
 
 cd ../../../reader/harmattan/l10n
 
-for LANG in de es fr it zh_CN
+for LANG in de es fr it ru zh_CN
 do
 lrelease ocnewsreader_$LANG.ts
 done
