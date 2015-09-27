09f48be9be71605ccbbe5ad3edffe0bc1df2d287
Buschmann <kontakt@buschmann23.de>
2015-09-27 14:31:46 +0200

    [Sailfish OS] added new languages to language chooser

--- reader/common/models/oclanguagemodel.cpp
+++ reader/common/models/oclanguagemodel.cpp
@@ -56,14 +56,16 @@ QModelIndex OcLanguageModel::index(int row, int column, const QModelIndex &paren
 
 void OcLanguageModel::init()
 {
-    m_langs << "C" << "en_GB" << "da" << "de" << "el" << "es" << "fr" << "it" << "nl_NL" << "ru" << "zh_CN";
+    m_langs << "C" << "en_GB" << "da" << "de" << "el" << "es" << "fi" << "fr" << "gl" << "it" << "nl_NL" << "ru" << "zh_CN";
     m_langCode["C"] = tr("System default");
     m_langCode["en_GB"] = tr("English");
     m_langCode["da"] = tr("Danish");
     m_langCode["de"] = tr("German");
     m_langCode["el"] = tr("Greek");
     m_langCode["es"] = tr("Spanish");
+    m_langCode["fi"] = tr("Finnish");
     m_langCode["fr"] = tr("French");
+    m_langCode["gl"] = tr("Galician");
     m_langCode["it"] = tr("Italian");
     m_langCode["nl_NL"] = tr("Dutch (Netherlands)");
     m_langCode["ru"] = tr("Russian");
